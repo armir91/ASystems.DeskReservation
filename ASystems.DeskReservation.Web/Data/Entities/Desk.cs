32d27e9cbc2c32a690509809953a2541925b719e
Armir <armir.keta@gmail.com>
2023-12-04 09:49:05 +0100

    Added Desk Name Annotations

--- ASystems.DeskReservation.Web/Data/Entities/Desk.cs
+++ ASystems.DeskReservation.Web/Data/Entities/Desk.cs
@@ -1,8 +1,14 @@
-﻿namespace ASystems.DeskReservation.Web.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASystems.DeskReservation.Web.Data.Entities;
 
 public class Desk
 {
     public Guid Id { get; set; }
+
+    [Required]
+    [StringLength(6, ErrorMessage = "Name cannot be longer than 6 characters")]
+    [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Special characters are not allowed.")]
     public string Name { get; set; }
 
     public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
