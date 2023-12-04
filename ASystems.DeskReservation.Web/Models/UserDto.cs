d91aca37852e970f1273fded77376cad1265866e
Armir <armir.keta@gmail.com>
2023-12-04 09:58:39 +0100

    Added annotations in UserDto

--- ASystems.DeskReservation.Web/Models/UserDto.cs
+++ ASystems.DeskReservation.Web/Models/UserDto.cs
@@ -1,9 +1,19 @@
-﻿namespace ASystems.DeskReservation.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASystems.DeskReservation.Web.Models
 {
     public class UserDto
     {
         public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Name cannot be longer than 15 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Special characters are not allowed.")]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Name cannot be longer than 15 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Special characters are not allowed.")]
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
